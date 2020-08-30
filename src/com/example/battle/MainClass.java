44dc2c611fd178b432a1e790164357e76a1d91ca
प्रवेश कुमार असिवाल <29462369+praveshasiwal@users.noreply.github.com>
2020-08-30 11:29:16 +0530

    Update MainClass.java

--- src/com/example/battle/MainClass.java
+++ src/com/example/battle/MainClass.java
@@ -17,18 +17,29 @@ public class MainClass {
 		Scanner sc = new Scanner(System.in);
 		//Enter Battle Ground
 		extractBattleGround(sc);	
-		
+		//Enter Ship Count
 		System.out.print("Enter Ship counts : ");
 		int shipCount = sc.nextInt();
+		//Enter Ship Placement and Place them as well
+		extractedShipInformation(sc, shipCount);
 		
+		
+		System.out.println("");
+	}
+
+	private static void extractedShipInformation(Scanner sc, int shipCount) {
 		for (int i = 0; i < shipCount; i++) {
+			
+			System.out.print("Please enter ship type (X OR Y) : ");
+			ShipType shipType = ShipType.valueOf(sc.next());			
+			
 			System.out.println("Please Enter Ship Matrix Size");
 			
 			System.out.print("Please enter row count : ");
 			int shipX = sc.nextInt();
 			
 			System.out.print("Please enter col count : ");
-			int shipY = sc.nextInt();
+			int shipY = sc.nextInt();			
 			
 			System.out.print("Please enter x cordinate : ");
 			int shipXCordinate = sc.nextInt();
@@ -36,12 +47,30 @@ public class MainClass {
 			System.out.print("Please enter y cordinate : ");
 			int shipYCordinate = sc.nextInt();			
 			
-			System.out.print("Please enter ship type (X OR Y) : ");
-			ShipType shipType = ShipType.valueOf(sc.next());
+			extractedShipPlacement(shipType, shipX, shipY, shipXCordinate, shipYCordinate);			
 			
 		}
-		
-		System.out.println("");
+	}
+
+	private static void extractedShipPlacement(ShipType shipType, int shipX, int shipY, int shipXCordinate,
+			int shipYCordinate) {
+		for(int k = shipXCordinate; k <= (shipX + shipXCordinate); k++) {
+			for(int l = shipYCordinate; l <= (shipY + shipYCordinate); l++) {
+				
+				if(!matrix.containsKey(k+"-"+l)) {
+					break;
+				}else if(matrix.get(k+"-"+l) > 0) {
+					break;
+				}else {
+					if(shipType == ShipType.X) {
+						matrix.put(k+"-"+l, 1);
+					} else {
+						matrix.put(k+"-"+l, 2);
+					}
+				}
+				
+			}
+		}
 	}
 
 	private static void extractBattleGround(Scanner sc) {
